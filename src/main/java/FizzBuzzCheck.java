f10f74064537a2cee7a801c20b17e9b7dbc18f95
frankyang <frank4yang@163.com>
2019-12-28 13:42:45 +0800

    Java_杨凡_成都项目建设处

--- src/main/java/FizzBuzzCheck.java
+++ src/main/java/FizzBuzzCheck.java
@@ -1,13 +1,27 @@
 public class FizzBuzzCheck {
     public static String checkFizzBuzz(int num) {
-        if (num % 3 == 0 && num % 5 == 0) {
-            return "FizzBuzz";
-        } else if (num % 5 == 0) {
-            return "Buzz";
-        } else if (num % 3 == 0) {
-            return "Fizz";
+        String s = "";
+        if (num > 0) {
+            if (num % 3 == 0 && num % 5 == 0 && num % 7 == 0) {
+                s += "FizzBuzzWhizz";
+            } else if (num % 3 == 0 && num % 5 == 0) {
+                s += "FizzBuzz";
+            } else if (num % 3 == 0 && num % 7 == 0) {
+                s += "FizzWhizz";
+            } else if (num % 5 == 0 && num % 7 == 0) {
+                s += "BuzzWhizz";
+            } else if (num % 7 == 0) {
+                s += "Whizz";
+            } else if (num % 5 == 0) {
+                s += "Buzz";
+            } else if (num % 3 == 0) {
+                s += "Fizz";
+            } else {
+                s += num;
+            }
+            return s;
         } else {
-            return num + "";
+            throw new IllegalArgumentException("Invalid input");
         }
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,27 +1,31 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
 import java.util.Arrays;
 import java.util.Collection;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 
 @RunWith(Parameterized.class)
 public class FizzBuzzTest {
-    //shift+enter,ctrl+n,generate
     private int num;
     private String ans;
+
     public FizzBuzzTest(int num, String ans) {
         this.num = num;
         this.ans = ans;
     }
 
     @Parameterized.Parameters
-    public static Collection<Object []> getParams(){
+    public static Collection<Object[]> getParams() {
         return Arrays.asList(new Object[][]{
-                {3,"Fizz"},
-                {5,"Buzz"},
-                {15,"FizzBuzz"},
-                {1,"1"}
+                {3, "Fizz"},
+                {5, "Buzz"},
+                {15, "FizzBuzz"},
+                {1, "1"}
         });
 
     }
@@ -30,5 +34,16 @@ public class FizzBuzzTest {
     public void should_return_fizz_buzz_given_number_divisible_by_param() {
         assertThat(FizzBuzzCheck.checkFizzBuzz(num)).isEqualTo(ans);
     }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void should_throw_exception_given_num_le_0() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Invalid input");
+        FizzBuzzCheck.checkFizzBuzz(-2);
+    }
+
 
 }
